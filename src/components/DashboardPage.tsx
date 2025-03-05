292a08071da1471450cc789b3bc0d7cba579776b
DOMinator.jsx <parthbrajput30@gmail.com>
2025-03-06 00:57:50 +0530

    Side bar created

--- src/components/DashboardPage.tsx
+++ src/components/DashboardPage.tsx
@@ -18,15 +18,17 @@ function DashboardPage({
 }: IDashboardPage) {
   return (
     <section className="flex-1 h-full w-full flex flex-col">
-      <div className="p-6 sm:p-8 flex justify-between border-b border-gray-200">
-        <div className="flex flex-col sm:flex-row sm:items-center gap-y-2  gap-x-8">
+      <div className="p-6 w-full sm:p-8 flex justify-between border-b border-gray-200">
+        <div className="flex w-full flex-col items-start sm:flex-row sm:items-center gap-y-6">
+          <div className="flex items-center gap-8">
           {hideBackButton ? null : (
             <Button className="w-fit  bg-white" variant={"outline"}>
               <ArrowLeft className="size-4" />
             </Button>
           )}
           <Heading>{title}</Heading>
-          {cta ? <div>{cta}</div> : null}
+          </div>
+          {cta ? <div className="w-full">{cta}</div> : null}
         </div>
       </div>
       <div className="flex-1 p-6 sm:p-8 flex flex-col overflow-y-auto">
