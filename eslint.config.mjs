393c993da3f3e36765625c137c9f76466c5bdd67
DOMinator.jsx <parthbrajput30@gmail.com>
2025-03-08 13:00:21 +0530

    new-update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,9 +16,10 @@ const eslintConfig = [
       "react/no-unescaped-entities": "off",
       "@next/next/no-img-element": "off",
       "no-unused-expressions": "off",
-      "@typescript-eslint/no-explicit-any": "off",  
-      "@typescript-eslint/no-unused-vars": "off",  
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/no-unused-expressions": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
     },
   }),
 ];
